798cb2ee44d09961a81ee33968981bed4e27545f
camarcano <c.marcano@gmail.com>
2024-11-02 12:30:11 -0600

    .

--- app/routes/webhook.py
+++ app/routes/webhook.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request, jsonify
 import os
 import subprocess
-import stat
 
 webhook_bp = Blueprint('webhook', __name__)
 
@@ -11,13 +10,17 @@ REPO_BRANCH = '26-create-streamlit-app-designed-for-open-source-annotations'
 
 @webhook_bp.route('/webhook', methods=['POST'])
 def webhook():
-    # Get the branch name from the request (default to the specified branch if not provided)
+    # Get the branch name from the request (default to 'main' if not provided)
     branch = request.args.get('branch', REPO_BRANCH)
 
-    # Remove the existing repository directory if it exists
+    # Check if the repository directory exists
     if os.path.exists(REPO_DIR):
+        # Change permissions recursively to allow deletion
+        subprocess.run(["chmod", "-R", "775", REPO_DIR], check=True)
+        
+        # Remove the existing repository directory
         print(jsonify({"message": "Trying to delete existing repo."}))
-        subprocess.run(["rm", "-rf", REPO_DIR])
+        subprocess.run(["rm", "-rf", REPO_DIR], check=True)
         print(jsonify({"message": "Existing repo deleted"}))
 
     # Clone the specified branch of the repository
@@ -26,12 +29,5 @@ def webhook():
 
     if clone_result.returncode != 0:
         return jsonify({"error": clone_result.stderr, "message": "Clone failed"}), 500
-
-    # Set permissions for the cloned directory and files
-    for root, dirs, files in os.walk(REPO_DIR):
-        for dir in dirs:
-            os.chmod(os.path.join(root, dir), stat.S_IRWXU | stat.S_IRWXG | stat.S_IROTH | stat.S_IXOTH)  # 775
-        for file in files:
-            os.chmod(os.path.join(root, file), stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH)  # 664
 
     return jsonify({"message": "Repository successfully cloned", "branch": branch}), 200
