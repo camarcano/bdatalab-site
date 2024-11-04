c6f8be614ae4ed39f2df3dea8f99ea48923bb872
camarcano <c.marcano@gmail.com>
2024-11-03 22:18:40 -0600

    .

--- app/blueprints/admin.py
+++ app/blueprints/admin.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
 
-@admin_bp.route('/admin')
+@admin_bp.route('/')
 def admin_home():
     return render_template('admin/dashboard.html', title="Admin Dashboard")
